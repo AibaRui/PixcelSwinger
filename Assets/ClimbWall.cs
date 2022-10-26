ec1df279db14307bd16689d78f1526106e47e9ab
Aiba_0325 <bantan.ab325@gmail.com>
2022-10-26 23:52:42 +0900

    no message

--- Assets/ClimbWall.cs
+++ Assets/ClimbWall.cs
@@ -10,11 +10,19 @@ public class ClimbWall : MonoBehaviour
     [SerializeField] float _boxCastY = 1;
     [SerializeField] float _boxCastZ = 1;
 
+    [SerializeField] float _downLong;
+
+    [SerializeField] float _upLayerLong;
+
     [SerializeField] Vector3 posAdd;
     /// <summary>前方に壁があるかどうか</summary>
     bool _fowardWall;
+
     /// <summary>登る壁の上に壁があるかどうか</summary>
     bool _UpWall;
+
+    bool _downWall;
+
     RaycastHit hitFowardWal;
 
    public bool _isClimb;
@@ -59,14 +67,22 @@ public class ClimbWall : MonoBehaviour
 
         if(_isClimb)
         {
+            if(_downWall)
+            {
             Vector3 velo = new Vector3(0, 20, 0);
             _rb.velocity = velo;
-
-            if(!_fowardWall)
+            }
+            else
             {
                 _rb.velocity = Vector3.zero;
                 _rb.AddForce(transform.forward * 3, ForceMode.Impulse);
                 _isClimb = false;
+            }
+
+
+            if(!_fowardWall)
+            {
+
             }
         }
 
@@ -76,10 +92,15 @@ public class ClimbWall : MonoBehaviour
     public void CheckFowardWall()
     {
         Vector3 start = this.transform.position + transform.up * 2;
-        Vector3 end = start + transform.forward;
+        Vector3 end = start + transform.forward* _upLayerLong;
+
+        Vector3 start2 = this.transform.position + -transform.up * _downLong;
+        Vector3 end2 = start2 + transform.forward * _upLayerLong;
 
         _UpWall = Physics.Linecast(start, end,_layer); // 引いたラインに何かがぶつかっていたら true とする
         _fowardWall = Physics.BoxCast(transform.position + transform.forward + posAdd, new Vector3(_boxCastX, _boxCastY, _boxCastZ), transform.forward, out hitFowardWal, Quaternion.identity, 1.0f,_layer);
+
+        _downWall = Physics.Linecast(start2, end2, _layer);
 
         Debug.DrawLine(start, end, Color.green); // 動作確認用に Scene ウィンドウ上で線を表示する
     }
@@ -88,5 +109,16 @@ public class ClimbWall : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position + transform.forward+posAdd, new Vector3(_boxCastX, _boxCastY, _boxCastZ));
+
+
+        Vector3 start = this.transform.position + transform.up * 2;
+        Vector3 end = start + transform.forward* _upLayerLong;
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(start, end);
+
+        Vector3 start2 = this.transform.position + -transform.up * _downLong;
+        Vector3 end2 = start2 + transform.forward * _upLayerLong;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(start2, end2);
     }
 }
--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//using DG.Tweening;  // DOTween を使うため
+using DG.Tweening;  // DOTween を使うため
 
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] Text _scoreText;
+
+    int _score = 0;
+
     void Start()
     {
-        
+        _scoreText.text = _score.ToString();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
@@ -22,27 +25,27 @@ public class ScoreManager : MonoBehaviour
 
 
 
-//   public void SetScore()
-//    {
-//        int tempScore = _player2Score; // 追加前のスコア
+    public void SetScore(int addScore)
+    {
+        int tempScore = _score; // 追加前のスコア
 
 
-//        _player2Score += addScore;
-//        if (_player2Score >= 0)
-//        {
-//            // DOTween.To() を使って連続的に変化させる
-//            DOTween.To(() => tempScore, // 連続的に変化させる対象の値
-//                x => tempScore = x, // 変化させた値 x をどう処理するかを書く
-//                _player2Score, // x をどの値まで変化させるか指示する
-//                0.5f)   // 何秒かけて変化させるか指示する
-//                .OnUpdate(() => _scoreP2.text = tempScore.ToString())   // 数値が変化する度に実行する処理を書く
-//                .OnComplete(() => _scoreP2.text = _player2Score.ToString());   // 数値の変化が完了した時に実行する処理を書く
-//        }
-//        else
-//        {
-//            _player2Score = 0;
-//        }
-//        _scoreP2.text = _player2Score.ToString();
-//    }
-//}
+        _score += addScore;
+        if (_score >= 0)
+        {
+            // DOTween.To() を使って連続的に変化させる
+            DOTween.To(() => tempScore, // 連続的に変化させる対象の値
+                x => tempScore = x, // 変化させた値 x をどう処理するかを書く
+                _score, // x をどの値まで変化させるか指示する
+                0.5f)   // 何秒かけて変化させるか指示する
+                .OnUpdate(() => _scoreText.text = tempScore.ToString())   // 数値が変化する度に実行する処理を書く
+                .OnComplete(() => _scoreText.text = _score.ToString());   // 数値の変化が完了した時に実行する処理を書く
+        }
+        else
+        {
+            _score = 0;
+        }
+        _scoreText.text = _score.ToString();
+    }
 }
+
